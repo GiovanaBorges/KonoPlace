21035a1d3b7561a21bf08e0ad6ab02680e9d7bc2
thainacolli <thaina.colli@outlook.com>
2022-06-23 16:10:02 -0300

    upgrade color switcher

---
+++ js/js.js
@@ -0,0 +1,52 @@
+function validar(){
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const cargo = document.getElementById("job").value
+    const departamento = document.getElementById("departamento").value
+    const telefone = document.getElementById("phone").value
+    const senha = document.getElementById("pass").value
+
+    
+    
+    if(email === "" || email === null ||
+     name === "" || name=== null ||
+      cargo === "" || cargo=== null 
+      || departamento === "" 
+    || departamento=== null ||
+     telefone === "" || telefone=== null 
+     || senha === "" || senha === null
+    ){
+        alert("Não pode haver espaços vazios")
+    }
+
+}
+
+function validarcadastro(){ 
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const cargo = document.getElementById("cargo").value
+    const departamento = document.getElementById("departamento").value
+    const telefone = document.getElementById("phone").value
+    const senha = document.getElementById("pass").value
+    const confirmarsenha = document.getElementById("pass-confirm").value
+
+ 
+    
+    if(email === "" || email === null ||
+     name === "" || name=== null ||
+      cargo === "" || cargo=== null 
+      || departamento === "" 
+    || departamento=== null ||
+     telefone === "" || telefone=== null 
+     || senha === "" || senha === null
+     || confirmarsenha === "" || confirmarsenha === null 
+    ){
+        alert("Não pode haver campos vazios")
+    }
+
+    if(senha !== confirmarsenha){
+        alert("Senhas incompatíveis")
+
+    }
+
+}
