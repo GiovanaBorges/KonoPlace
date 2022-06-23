f5f3aaf1c8a35d6bff8729e9c087c7e7f2a1374a
gustavo <gsouzamascarenhas14@gmail.com>
2022-06-23 12:42:13 -0300

    remove and upgrade files

--- js/trocarcordefundo.js
+++ js/trocarcordefundo.js
@@ -2,7 +2,7 @@
 function sun(){
     document.querySelector(":root").style.setProperty("--bg" , "white")
     document.querySelector(":root").style.setProperty("--text" , "black")
-    document.querySelector(":root").style.setProperty("--gray-30" , "#DCD8D8")
+    document.querySelector(":root").style.setProperty("--gray-50" , "#DCD8D8")
     document.querySelector(":root").style.setProperty("--dark-100" , "#DCD8D8")
     
 }
@@ -10,10 +10,8 @@ function sun(){
 function moon(){
     document.querySelector(":root").style.setProperty("--bg" , "#303030")
     document.querySelector(":root").style.setProperty("--text" , "white")
-    document.querySelector(":root").style.setProperty("--gray-30" , "#302C2C")
+    document.querySelector(":root").style.setProperty("--gray-50" , "#302C2C")
     document.querySelector(":root").style.setProperty("--dark-100" , "#302C2C")
-    
-    
 }
 
 
